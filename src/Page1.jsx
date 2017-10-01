ac277fbe01ce338e23f2e990f5bc1f4ff2ae4018
Malick DIOP <diop-malick@hotmail.fr>
2017-10-01 17:21:20 +0000

    Tuto 1 - Développement JS moderne : REACT.js, FLUX, REDUX et GraphQL
    yarn add redux react-redux react-router-dom react-router-redux@next redux-thunk

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,54 +1,99 @@
 import React from 'react';
+import { createStore, combineReducers } from 'redux';
+import ReactDOM from 'react-dom';
+
+// import { createStore, applyMiddleware, compose } from 'redux'
+// import { routerMiddleware } from 'react-router-redux'
 
 /**
  * Développement JS moderne : REACT.js, FLUX, REDUX et GraphQL
  * https://www.youtube.com/watch?v=F0Ral7AJ1rs
  */
 
-const store = { products: ['P1', 'P2'], users: [] };
+/**
+ * Store
+ */
+const initialState2 = [
+  {
+    products: [{ id: 0, name: 'P1', quantity: 0 }, { id: 1, name: 'P2', quantity: 0 }],
+    users: [],
+  },
+];
+const initialState = [{ id: 0, name: 'P1', quantity: 0 }, { id: 1, name: 'P2', quantity: 0 }];
 
+// reducer : une fonction qui permet de modifier le store
+// state : donée du store initiale
+// return nouvelle données
+function products(state = initialState, action) {
+  console.log(action);
+  if (action.type === 'ADD_TO_CART') {
+    state[action.index].quantity += 1;
+    return state;
+  }
+  return state;
+}
+
+const store = createStore(
+  combineReducers({
+    products,
+  }),
+);
+
+// dispatch : send action to store
+
+/* 
+    Components
+ */
+function Header() {
+  return (
+    <div>
+      <h1>Product in cart : 0</h1>
+    </div>
+  );
+}
 function Page1() {
   return (
     <div>
-      <h1>Page 1</h1>
-      {store.products.map(product => <Product name={product} />)}
+      <Header />
+      {store
+        .getState()
+        .products.map(product => (
+          <Product name={product.name} quantity={product.quantity} id={product.id} />
+        ))}
     </div>
   );
 }
 
 export default Page1;
 
+console.log(store);
+
 // /////////////////////////////////////////
-
-class Product extends React.Component {
-  state = {
-    name: '',
-    quantity: 0,
-  };
-
-  //     constructor(props) {
-  //     super(props);
-  //     this.state = {
-  //       name: '',
-  //       quantity: 0,
-  //     };
-  //   }
-
-  addToCart() {
-    this.setState({
-      quantity: this.state.quantity + 1,
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        {/*  jhhjkh */}
-        <br />
-        <h2>Product {this.props.name}</h2>
-        <p>Quantity: {this.state.quantity}</p>
-        <button onClick={() => this.addToCart()}>Add to cart</button>
-      </div>
-    );
-  }
+function Product(props) {
+  return (
+    <div>
+      {/*  coments */}
+      <br />
+      <h2>Product {props.name}</h2>
+      <p>Quantity: {props.quantity}</p>
+      {/* au click, on envoyer event sur le store */}
+      <button
+        onClick={() =>
+          store.dispatch({
+            type: 'ADD_TO_CART',
+            index: props.id,
+          })}
+      >
+        Add to cart
+      </button>
+    </div>
+  );
 }
+
+function render() {
+  ReactDOM.render(<Page1 />, document.getElementById('root'));
+}
+
+// placer un écouterur pour voir ls changement sur le store
+// met à jour l'app dès qu'il y'a modif sur le store
+store.subscribe(render);
