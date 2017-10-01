3378e6bde82cf1af31b7bc264ddeb66f1196ca66
Malick DIOP <diop-malick@hotmail.fr>
2017-10-01 16:02:44 +0000

    Tuto 1 - Développement JS moderne : REACT.js, FLUX, REDUX et GraphQL
    https://www.youtube.com/watch?v=F0Ral7AJ1rs

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,5 +1,54 @@
 import React from 'react';
 
-const Page1 = () => <p>Page 1</p>;
+/**
+ * Développement JS moderne : REACT.js, FLUX, REDUX et GraphQL
+ * https://www.youtube.com/watch?v=F0Ral7AJ1rs
+ */
+
+const store = { products: ['P1', 'P2'], users: [] };
+
+function Page1() {
+  return (
+    <div>
+      <h1>Page 1</h1>
+      {store.products.map(product => <Product name={product} />)}
+    </div>
+  );
+}
 
 export default Page1;
+
+// /////////////////////////////////////////
+
+class Product extends React.Component {
+  state = {
+    name: '',
+    quantity: 0,
+  };
+
+  //     constructor(props) {
+  //     super(props);
+  //     this.state = {
+  //       name: '',
+  //       quantity: 0,
+  //     };
+  //   }
+
+  addToCart() {
+    this.setState({
+      quantity: this.state.quantity + 1,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {/*  jhhjkh */}
+        <br />
+        <h2>Product {this.props.name}</h2>
+        <p>Quantity: {this.state.quantity}</p>
+        <button onClick={() => this.addToCart()}>Add to cart</button>
+      </div>
+    );
+  }
+}
