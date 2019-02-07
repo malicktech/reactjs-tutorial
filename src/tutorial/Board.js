694be4e3224dc3c7e933d23e537ce6116f0b8208
Malick DIOP <diop-malick@hotmail.fr>
2019-02-07 03:33:17 +0000

    Tutorial - Making an Interactive Component

--- src/tutorial/Board.js
+++ src/tutorial/Board.js
@@ -3,8 +3,29 @@ import './index.css';
 import Square from './Square';
 
 class Board extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+          squares: Array(9).fill(null),
+        };
+    }
+
+    handleClick(i) {
+        // immutability 
+        // to create a copy of the squares array to modify instead of modifying the existing array.
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-      return <Square value={i} />;
+      return (
+      <Square 
+        value={this.state.squares[i]} 
+        onClick={() => this.handleClick(i)}
+      />
+      );
     }
   
     render() {
